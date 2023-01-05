04082f89209c3fb5bd54e097727cf8bfbc05132f
wnicolass <nicolaswgl99@gmail.com>
2023-01-05 21:54:18 +0000

    add signin and signout routes

--- routes.js
+++ routes.js
@@ -9,5 +9,7 @@ route.get("/", HomeController.index);
 //login routes
 route.get("/login/index", LoginController.index);
 route.post("/login/register", LoginController.register);
+route.post("/login/signin", LoginController.signIn);
+route.get("/login/signout", LoginController.signOut);
 
 module.exports = route;
