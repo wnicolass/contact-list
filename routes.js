8384bb6ba854f4a5959d9c1298e3bf93b0ebd77e
wnicolass <nicolaswgl99@gmail.com>
2023-01-12 14:25:57 +0000

    feat: create view for add contacts

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const route = express.Router();
 const HomeController = require("./src/controllers/HomeController");
 const LoginController = require("./src/controllers/LoginController");
+const ContactController = require("./src/controllers/ContactController");
 
 //home routes
 route.get("/", HomeController.index);
@@ -11,5 +12,8 @@ route.get("/login/index", LoginController.index);
 route.post("/login/register", LoginController.register);
 route.post("/login/signin", LoginController.signIn);
 route.get("/login/signout", LoginController.signOut);
+
+//contact routes
+route.get("/contact/index", ContactController.index);
 
 module.exports = route;
